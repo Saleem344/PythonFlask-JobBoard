7cc9d3ba685b9c06f81bb6eaca125aab208ffa94
saleemshaik344 <saleemshaik344@gmail.com>
2022-02-01 09:52:07 +0530

    initial commit

---
+++ jobs/app.py
@@ -0,0 +1,10 @@
+from re import A
+from flask import Flask
+from flask import render_template
+
+app =Flask(__name__)
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
